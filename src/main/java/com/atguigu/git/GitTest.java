ef3d4e9d574b66e6838359c5ab966cd37b45abcc
xiedingjie <3254050042@qq.com>
2022-12-09 17:37:01 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 解丁杰
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}

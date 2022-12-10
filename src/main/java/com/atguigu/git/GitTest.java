adf10b1187cfbed41ca8ab4ccb5af0789a1f2819
解瑞 <3254050042@qq.com>
2022-12-10 08:27:07 +0000

    update src/main/java/com/atguigu/git/GitTest.java. upGitTest
    Signed-off-by: 解瑞 <3254050042@qq.com>

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git5 hot-fix");
         System.out.println("hello git6 master");
         System.out.println("hello gitee");
+        System.out.println("hello gitee2");
     }
 }
